d8824d6a0a61ffb2c05ed83f03dc5353a5fc0e7c
Eduardo <eduardomax27@outlook.com>
2024-06-20 17:15:40 -0300

    Feat: Beggining the project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
-import './App.css';
-
+import './App.scss';
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+import { Main } from './components/Main';
+import { Aside } from './components/Aside';
+import { Nav } from './components/Nav';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header>
+
+      </Header>
+  
+      <div className='container'>
+        <Main>
+        </Main>
+
+        <Aside>
+        </Aside>
+      </div>
+
+      <Footer>
+      </Footer>
     </div>
   );
 }
